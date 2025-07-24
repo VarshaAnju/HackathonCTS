b2642e45ce9f0a168608e5739e3b31334c548f71
Varsha <2408545@cognizant.com>
2025-07-24 10:51:16 +0530

    First Changes Commit

--- src/test/java/com/cts/hackathonproject/pageobjects/ExplorePage2.java
+++ src/test/java/com/cts/hackathonproject/pageobjects/ExplorePage2.java
@@ -2,17 +2,16 @@ package com.cts.hackathonproject.pageobjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v135.page.Page;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class ExplorePageThatFucksUpEverything extends BasePage{
+public class ExplorePage2 extends BasePage{
 
     private WebDriver driver;
 
-    public ExplorePageThatFucksUpEverything(WebDriver driver){
+    public ExplorePage2(WebDriver driver){
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver,this);
